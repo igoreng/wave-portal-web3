e4c9369570f02cfce20c5fd278e02251ea156f2f
Igor Silva <osilvaigor@gmail.com>
2022-04-20 23:39:03 -0300

    fix: adjust missing not logic

--- client/src/App.js
+++ client/src/App.js
@@ -104,7 +104,7 @@ const App = () => {
           I am Igor and I am learning how to make a web3 app because of my final
           college paper. In Brazil we call it TCC.
         </div>
-        {miningStatus && (
+        {!miningStatus && (
           <button className="waveButton" onClick={wave}>
             Wave at Me
           </button>
